fb097f737ae3858811ebfa9e20199ab6d71b9db0
JohnMorrisBerkleeOnline <41553512+JohnMorrisBerkleeOnline@users.noreply.github.com>
2018-08-30 12:36:53 -0500

    Add files via upload

---
+++ Assignment10Morris(database).sql
@@ -0,0 +1,25 @@
+create table setlist (
+  id      serial primary key,
+  songname varchar(100),
+  artistfirstname varchar(100),
+  artistlastname varchar(100),
+  style varchar(20),
+  key varchar(20)
+ );
+
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('miseryandgin', 'merle', 'haggard', 'ballad', 'C');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('drivingnailsinmycoffin', 'johnny', 'bush', 'westernswing', 'A');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('moteltimeagain', 'johnny', 'paycheck', 'shuffle', 'C');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('ifyoushouldcomebacktoday', 'charlie', 'pride', 'straight', 'D');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('lovebug', 'george', 'jones', 'straight', 'A');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('silverwings', 'merle', 'haggard', 'ballad', 'A');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('mywindowfacesthesouth', 'willie', 'nelson', 'westernswing', 'E');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('sittinandthinkin', 'ray', 'price', 'shuffle', 'C');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('hellotrouble', 'ernest', 'tubb', 'shuffle', 'E');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('brandedman', 'merle', 'haggard', 'straight', 'C');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('detour', 'hank', 'thompson', 'westernswing', 'A');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('illgodownswingin', 'porter', 'wagoner', 'shuffle', 'D');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('littleolewinedrinkerme', 'merle', 'haggard', 'straight', 'D');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('mr.recordman', 'willie', 'nelson', 'shuffle', 'F');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('fourwalls', 'jim', 'reeves', 'waltz', 'A');
+INSERT INTO setlist (songname, artistfirstname, artistlastname, style, key) VALUES ('winemeup', 'faron', 'young', 'shuffle', 'C');
